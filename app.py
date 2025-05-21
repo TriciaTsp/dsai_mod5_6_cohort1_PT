632d949cd90a886db18e5405e98acbba74d08efd
TriciaTsp <hanguksamantha@gmail.com>
2025-05-21 13:22:51 +0000

    1

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import markdown2
 import os
+import sqlite3, datetime
 
 # Load environment variables from .env file
 load_dotenv()
@@ -21,7 +22,18 @@ def index():
 
 @app.route("/gemini", methods=["GET","POST"])
 def gemini():
-    return(render_template("gemini.html"))
+    print("here")
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    df = c.execute("select * from users")
+    r = ""
+    username = ""
+    for row in c:
+        username = username + " | " + str(row)
+    c.close()
+    conn.close()
+    print("username: " + username)
+    return(render_template("gemini.html", r=username))
 
 @app.route("/gemini_reply", methods=["GET","POST"])
 def gemini_reply():
@@ -30,6 +42,60 @@ def gemini_reply():
     r = model.generate_content(q)
     r = markdown2.markdown(r.text)
     return(render_template("gemini_reply.html", r=r))
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    if request.method == "POST":
+        username = request.form.get("q")
+        t = datetime.datetime.now()
+
+        conn = sqlite3.connect("user.db")
+        c = conn.cursor()
+        c.execute("insert into users values(?, ?)",(username,t))
+        conn.commit()
+
+        # Fetch username
+        c.execute("SELECT name FROM users ORDER BY timestamp DESC LIMIT 1")
+        latest_user = c.fetchone()[0]
+
+        c.close()
+        conn.close()
+    
+    #return(render_template("gemini.html", usernames=latest_user))
+    return(render_template("main.html"))
+
+@app.route("/user_log", methods=["GET","POST"])
+def user_log():
+    t = datetime.datetime.now()
+
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    # Fetch all username
+    c.execute("SELECT * FROM users")
+    users = c.fetchall()
+    print("here")
+    c.close()
+    conn.close()
+    
+    return(render_template("user_log.html", usernames=users))
+
+@app.route("/delete_log", methods=["GET","POST"])
+def delete_log():
+    t = datetime.datetime.now()
+
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    # Fetch all username
+    c.execute("delete from users")
+    conn.commit()
+    c.close()
+    conn.close()
+    
+    return(render_template("delete_log.html"))
+
+@app.route("/paynow", methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 
 if __name__ == "__main__":
     app.run()
